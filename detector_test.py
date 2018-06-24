b37cf11fc18af66e8c2f0683cf4fb6f19b6489cc
zhichengMLE <jason-dean@outlook.com>
2018-06-24 14:05:14 -0400

    Update webcam and video support

--- detector_test.py
+++ detector_test.py
@@ -4,10 +4,10 @@ if __name__ == '__main__':
     det = Detecor_Selector(detector_name='mobilenet_ssd', config_path='detectors.cfg')
 
     # detect_image_test
-    det.detect_image('./_samples/MOT17-09-FRCNN/img1/000001.jpg')
+    #det.detect_image('./_samples/MOT17-09-FRCNN/img1/000001.jpg')
 
     # detect_video_test
-    #det.detect_video('./_samples/MOT17-09-FRCNN,mp4')
+    #det.detect_video('./_samples/MOT17-09-FRCNN.mp4')
 
     # detect_webcam_test
-    #det.detect_webcam()
+    det.detect_webcam()
