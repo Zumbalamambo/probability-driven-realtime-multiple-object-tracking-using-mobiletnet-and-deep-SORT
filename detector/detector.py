a4c9692d1803cbe6e93d4622c20ec95f478cf1c9
zhichengMLE <jason-dean@outlook.com>
2018-06-24 13:27:44 -0400

    Add detector template

---
+++ detector/detector.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 10:28:15 2018
+
+@author: Administrator
+"""
+
+class Detector(object):
+    def __init__(self):
+        pass
+
+    def _image_preprocessing(self, image_frame):
+        pass
+
+    def _image_postprocessing(self, image_frame):
+        pass
+
+    def detect_image(self, image_path):
+        pass
+
+    def detect_video(self, video_path):
+        pass
+
+    def detect_webcam(self):
+        pass
