9667add7cd051ad30054c996f1ee3c56c136f212
zhichengMLE <jason-dean@outlook.com>
2018-06-24 13:28:11 -0400

    Add detctor test

---
+++ detector_test.py
@@ -0,0 +1,13 @@
+from detector.detector_selector import Detecor_Selector
+
+if __name__ == '__main__':
+    det = Detecor_Selector(detector_name='mobilenet_ssd', config_path='detectors.cfg')
+
+    # detect_image_test
+    det.detect_image('./_samples/MOT17-09-FRCNN/img1/000001.jpg')
+
+    # detect_video_test
+    #det.detect_video('./_samples/MOT17-09-FRCNN,mp4')
+
+    # detect_webcam_test
+    #det.detect_webcam()
