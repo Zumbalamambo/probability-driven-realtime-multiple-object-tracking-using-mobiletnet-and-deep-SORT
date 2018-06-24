35b7456e8c639933d4c289c5ba687290aba8544b
zhichengMLE <jason-dean@outlook.com>
2018-06-24 17:19:40 -0400

    Add tracker template

---
+++ tracker/tracker_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 24 14:39:59 2018
+
+@author: Administrator
+"""
+
+class Tracker_Template(object):
+    def __init__(self):
+        pass
+
+    def _get_detections(self):
+        pass
+
+    def start_tracking(self):
+        pass
