2239e7a318a62afd780090580c3bbcad4b83a3a7
zhichengMLE <jason-dean@outlook.com>
2018-06-25 21:58:11 -0400

    remove mobilenet

--- detector/detector_selector.py
+++ detector/detector_selector.py
@@ -6,7 +6,6 @@ Created on Sun Jun 24 10:29:25 2018
 """
 from .detector import Detector
 from .mobilenet_ssd import Mobilenet_Ssd
-from .mobilenet import Mobilenet
 
 class Detecor_Selector(Detector):
     def __init__(self, detector_name, config_path):
@@ -14,7 +13,6 @@ class Detecor_Selector(Detector):
 
     def _detector_selection(self, detector_name, config_path):
         detector_map = {'mobilenet_ssd' : Mobilenet_Ssd,
-                        'mobilenet' : Mobilenet
         }
 
         return detector_map[detector_name](config_path)
