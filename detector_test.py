3bf29bee0133014ce01c101cc8f7a3d54a8b7dbf
zhichengMLE <jason-dean@outlook.com>
2018-06-28 09:07:39 -0400

    Update detector test

--- detector_test.py
+++ detector_test.py
@@ -1,18 +1,18 @@
-from detector.detector_selector import Detecor_Selector
+from detector.detector import Detector
 
 if __name__ == '__main__':
-    det = Detecor_Selector(detector_name='mobilenet_ssd', config_path='detectors.cfg')
+    det = Detector(detector_name='mobilenet_ssd', config_path='detectors.cfg')
 
     # detect_image_test
-    #det.detect_image('./_samples/MOT17-09-FRCNN/img1/000001.jpg')
+    det.detect_image('./_samples/MOT17-09-FRCNN/img1/000055.jpg')
 
     # detect_video_test
-    det.detect_video('./_samples/MOT17-09-FRCNN.mp4')
+    #det.detect_video('./_samples/MOT17-09-FRCNN.mp4')
 
     # detect_webcam_test
     #det.detect_webcam()
-    
-    
-    
-    #det = Detecor_Selector(detector_name='mobilenet', config_path='detectors.cfg')
+
+
+
+    #det = Detector(detector_name='mobilenet', config_path='detectors.cfg')
     #det.detect_webcam()
