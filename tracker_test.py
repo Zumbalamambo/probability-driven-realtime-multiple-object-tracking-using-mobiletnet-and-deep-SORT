0d496905207c9b936838b289c09c19c2942609df
zhichengMLE <jason-dean@outlook.com>
2018-06-28 09:08:55 -0400

    Update tracker test

--- tracker_test.py
+++ tracker_test.py
@@ -5,8 +5,8 @@ Created on Sun Jun 24 16:23:31 2018
 @author: Administrator
 """
 
-from tracker.tracker_selector import Tracker_Selector
+from tracker.tracker import Tracker_temp
 
 if __name__ == "__main__":
-    track = Tracker_Selector(tracker_name='deep_sort', config_path='./trackers.cfg')
-    track.start_tracking()
+    tracker = Tracker_temp(tracker_name='deep_sort', config_path='./trackers.cfg')
+    tracker.start_tracking()
