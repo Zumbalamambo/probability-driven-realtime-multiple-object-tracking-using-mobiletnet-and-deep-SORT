384bbabb439417b5e6b622aa423e84c12c05ddc5
zhichengMLE <jason-dean@outlook.com>
2018-07-09 08:34:27 -0400

    Support confident threshold in each detector

--- detector/mobilenet_ssd.py
+++ detector/mobilenet_ssd.py
@@ -22,7 +22,7 @@ class Mobilenet_Ssd(object):
         self.detect_classes = config.get('mobilenet_ssd', 'detect_classes').split(',')
         self.ignore_classes = set(config.get('mobilenet_ssd', 'ignore_classes').split(','))
 
-    def detect_image(self, image_frame, height, width, to_xywh):
+    def detect_image(self, image_frame, height, width, to_xywh, confident_threshold):
         blob = cv2.dnn.blobFromImage(cv2.resize(image_frame, (self.image_height, self.image_width)), 0.007843, (self.image_height, self.image_width), (127.5, 127.5, 127.5), False)
         self.net.setInput(blob)
         detections = self.net.forward()
@@ -32,21 +32,22 @@ class Mobilenet_Ssd(object):
             confidence = detections[0, 0, i, 2]
             idx = int(detections[0, 0, i, 1])
 
-            if(self.detect_classes[idx] in self.ignore_classes):
-                continue
-            else:
-                bounding_box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
-                ret1, ret2, ret3, ret4 = bounding_box.astype("int")
-                if(to_xywh is True):
-                    ret1, ret2, ret3, ret4 = int(ret1), int(ret2), int(ret3-ret1), int(ret4-ret2)
+            if(confidence > confident_threshold):
+                if(self.detect_classes[idx] in self.ignore_classes):
+                    continue
+                else:
+                    bounding_box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+                    ret1, ret2, ret3, ret4 = bounding_box.astype("int")
+                    if(to_xywh is True):
+                        ret1, ret2, ret3, ret4 = int(ret1), int(ret2), int(ret3-ret1), int(ret4-ret2)
 
-                    if ret1 < 0 :
-                        ret3 += ret1
-                        ret1 = 0
-                    if ret2 < 0 :
-                        ret4 += ret2
-                        ret2 = 0
+                        if ret1 < 0 :
+                            ret3 += ret1
+                            ret1 = 0
+                        if ret2 < 0 :
+                            ret4 += ret2
+                            ret2 = 0
 
-                detection_results.append([self.detect_classes[idx], ret1, ret2, ret3, ret4, confidence])
+                    detection_results.append([self.detect_classes[idx], ret1, ret2, ret3, ret4, confidence])
 
         return detection_results
