7577cf1dd21bdbb15756700fe92392e9a5632abd
zhichengMLE <jason-dean@outlook.com>
2018-07-12 12:34:55 -0400

    Update trakcer_deep_SORT

--- tracker/tracker_deep_sort.py
+++ tracker/tracker_deep_sort.py
@@ -10,7 +10,7 @@ import configparser
 import numpy as np
 
 from .deep_sort.application_util import preprocessing, visualization
-from .deep_sort.deep_sort import nn_matching
+from .deep_sort.deep_sort import nn_matching, linear_assignment
 from .deep_sort.deep_sort.detection import Detection
 from .deep_sort.deep_sort.tracker import Tracker
 from .deep_sort.tools.generate_detections import create_box_encoder
@@ -85,66 +85,9 @@ class Tracker_Deep_Sort(Tracker_Template):
         self.tracker.update(detections)
 
         return self.tracker, detections
-    """
-    def start_tracking(self):
 
-        Run multi-target tracker on a particular sequence.
+    def is_detection_needed(self):
+        return linear_assignment.is_tracker_in_low_prob
 
-        seq_info = mot_challenge_util.gather_sequence_info(self.sequence_dir, self.detection_file)
-        metric = nn_matching.NearestNeighborDistanceMetric(
-            "cosine", self.max_cosine_distance, self.nn_budget)
-        tracker = Tracker(metric)
-        results = []
-
-        def frame_callback(vis, frame_idx):
-            start_time = time.time()
-            print("Processing frame %05d" % frame_idx)
-
-            # Load image and generate detections.
-            detections = self._create_detections(
-                seq_info["detections"], frame_idx, self.min_detection_height)
-            detections = [d for d in detections if d.confidence >= self.min_confidence]
-
-            # Run non-maxima suppression.
-            boxes = np.array([d.tlwh for d in detections])
-            scores = np.array([d.confidence for d in detections])
-            indices = preprocessing.non_max_suppression(
-                boxes, self.nms_max_overlap, scores)
-            detections = [detections[i] for i in indices]
-
-            # Update tracker.
-            tracker.predict()
-            tracker.update(detections)
-
-            # Update visualization.
-            if self.display:
-                image = cv2.imread(
-                    seq_info["image_filenames"][frame_idx], cv2.IMREAD_COLOR)
-                vis.set_image(image.copy())
-                vis.draw_detections(detections)
-                vis.draw_trackers(tracker.tracks)
-                vis.show_fps(0, 25, 'FPS: ' + str(round(1.0 / (time.time() - start_time), 1)))
-
-            # Store results.
-            for track in tracker.tracks:
-                if not track.is_confirmed() or track.time_since_update > 1:
-                    continue
-                bbox = track.to_tlwh()
-                results.append([
-                    frame_idx, track.track_id, bbox[0], bbox[1], bbox[2], bbox[3]])
-
-        # Run tracker.
-        if self.display:
-            visualizer = visualization.Visualization(seq_info, update_ms=5)
-        else:
-            visualizer = visualization.NoVisualization(seq_info)
-
-        visualizer.run(frame_callback)
-
-        if self.is_output is True:
-            # Store results.
-            f = open(self.output_file, 'w')
-            for row in results:
-                print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (
-                    row[0], row[1], row[2], row[3], row[4], row[5]),file=f)
-    """
+    def set_detecion_needed(self, value):
+        linear_assignment.is_tracker_in_low_prob = value
