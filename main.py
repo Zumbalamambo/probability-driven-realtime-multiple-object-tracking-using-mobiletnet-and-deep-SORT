e039f89dd1c452f9c135bd13ca59f10655cdf0c2
zhichengMLE <jason-dean@outlook.com>
2018-07-15 13:55:39 -0400

    Add MOT16 training sample and functional main function

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from __future__ import division, print_function, absolute_import
 
 from timeit import time
 import warnings
 import cv2
 import numpy as np
+
+from utils.bounding_box_transform import to_tlwh
+
 from detector.detector import Detector
 from tracker.tracker import Tracker_temp
 
-from tracker.deep_sort.application_util import preprocessing
-from tracker.deep_sort.deep_sort import nn_matching
-from tracker.deep_sort.deep_sort.detection import Detection
-from tracker.deep_sort.deep_sort.tracker import Tracker
-from tracker.deep_sort.tools.generate_detections import generate_detections as gdet
-from tracker.deep_sort.tools.generate_detections import create_box_encoder
 warnings.filterwarnings('ignore')
-
-
 DETECT_FREQUENCY = 6
 DOWN_SAMPLE_RATIO = 0.5
 IS_DETECTION_DISPLAY = False
 IS_TRACKING_DISPLAY = True
 
-if __name__ == '__main__':
-    det = Detector(detector_name='mobilenetv2_ssdlite', config_path='./detectors.cfg')
-    tra = Tracker_temp(tracker_name='deep_sort', config_path='./trackers.cfg')
+def tracking_by_detection(detector_name, tracker_name, videostream):
+    det = Detector(detector_name, config_path='./detectors.cfg')
+    tra = Tracker_temp(tracker_name, config_path='./trackers.cfg')
+    output_file = './_output/' + video_stream[video_stream.rfind('/')+1:video_stream.rfind('.')] + '.txt'
 
-    video_capture = cv2.VideoCapture('./_samples/MOT17-09.mp4')
-    #video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(video_stream)
     fps = 0.0
     step_counter = 0
     counter = 0
     first_time_flag = True
     start_time = time.time()
     total_time = time.time()
+    result_list = []
+    frame_index = 1
     while True:
         ret, frame = video_capture.read()
         if ret != True:
@@ -54,6 +50,8 @@ if __name__ == '__main__':
             for track in tracker.tracks:
                 if track.is_confirmed() and track.time_since_update > 3 :
                     continue
+                bbox = track.to_tlwh()
+                result_list.append([frame_index, track.track_id, bbox[1], bbox[0], bbox[2], bbox[3]])
                 bbox = track.to_tlbr()
                 cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
                 cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
@@ -78,10 +76,27 @@ if __name__ == '__main__':
                 first_time_flag = False
 
         cv2.imshow('image', frame)
+        frame_index += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     print('Average FPS:', round(counter / (time.time() - total_time), 1))
     print('Total eplased:', round(time.time() - total_time, 2))
+
+    try:
+        with open(output_file, 'w') as f:
+            for result in result_list:
+                #bounding_box = to_tlwh(np.array([result[2], result[3], result[4], result[5]]))
+                print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (int(result[0]), int(result[1]), result[2], result[3], result[4], result[5]),file=f)
+    except Exception as e:
+        print(e)
+        print('Something went wrong when writing output file!')
     video_capture.release()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    detector_name = 'mobilenetv2_ssdlite' 
+    tracker_name = 'deep_sort'
+    video_stream = './_samples/MOT16-09.mp4'
+
+    tracking_by_detection(detector_name, tracker_name, video_stream)
---
+++ tracker/utils/bounding_box_transform.py
@@ -0,0 +1,28 @@
+def to_tlwh(boundingbox):
+    """Get current position in bounding box format `(top left x, top left y,
+    width, height)`.
+
+    Returns
+    -------
+    ndarray
+        The bounding box.
+
+    """
+    ret = boundingbox
+    ret[2] *= ret[3]
+    ret[:2] -= ret[2:] / 2
+    return ret
+
+def to_tlbr(boundingbox):
+    """Get current position in bounding box format `(min x, miny, max x,
+    max y)`.
+
+    Returns
+    -------
+    ndarray
+        The bounding box.
+
+    """
+    ret = to_tlwh(boundingbox)
+    ret[2:] = ret[:2] + ret[2:]
+    return ret
