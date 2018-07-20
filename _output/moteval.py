025da66068571567b0aeba7c251e5a8b7da703d1
zhichengMLE <jason-dean@outlook.com>
2018-07-20 16:30:47 -0400

    Script for MOT15 train

--- _output/moteval.py
+++ _output/moteval.py
@@ -66,34 +66,51 @@ def compare_dataframes(gts, ts):
     return accs, names
 
 if __name__ == '__main__':
+    loglevel = 'info'
+    fmt = 'mot15-2D'
 
-    args = parse_args()
+    groundtruths = 'D:/_videos/2DMOT2015/train/'
+    tests_list = ['./MOT15_Train/squeeze_v1_0/', './MOT15_Train/mobilenetssd/', './MOT15_Train/mobilenetv2_ssdlite/']
+    #'./MOT15_Test/yolov3_tiny/', './MOT15_Test/mobilenetssd/', './MOT15_Test/mobilenetv2_ssdlite/', './MOT15_Test/squeeze_v1_0/']
+    case_list = ['vanilla', 'vanilla_down_sample', 'skip2', 'skip2_down_sample', 'skip2_with_prob', 'skip2_with_prob_down_sample', 'skip3', 'skip3_down_sample', 'skip3_with_prob', 'skip3_with_prob_down_sample', 'skip5', 'skip5_down_sample', 'skip5_with_prob', 'skip5_with_prob_down_sample', 'skip7', 'skip7_down_sample', 'skip7_with_prob', 'skip7_with_prob_down_sample']
 
-    loglevel = getattr(logging, args.loglevel.upper(), None)
+    combine_list = []
+    for test in tests_list:
+        for case in case_list:
+            combine_list.append(test + case + '/')
+
+    print(len(combine_list))
+
+    loglevel = getattr(logging, loglevel.upper(), None)
     if not isinstance(loglevel, int):
-        raise ValueError('Invalid log level: {} '.format(args.loglevel))        
+        raise ValueError('Invalid log level: {} '.format(loglevel))        
     logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s - %(message)s', datefmt='%I:%M:%S')
 
-    if args.solver:
-        mm.lap.default_solver = args.solver
+    for combine in combine_list:
+        gtfiles = glob.glob(os.path.join(groundtruths, '*/gt/gt.txt'))
+        tsfiles = [f for f in glob.glob(os.path.join(combine, '*.txt')) if not os.path.basename(f).startswith('eval') and os.path.basename(f).find('_result.txt') == -1]
 
-    gtfiles = glob.glob(os.path.join(args.groundtruths, '*/gt/gt.txt'))
-    tsfiles = [f for f in glob.glob(os.path.join(args.tests, '*.txt')) if not os.path.basename(f).startswith('eval')]
+        logging.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
+        logging.info('Available LAP solvers {}'.format(mm.lap.available_solvers))
+        logging.info('Default LAP solver \'{}\''.format(mm.lap.default_solver))
+        logging.info('Loading files.')
+        
+        gt = OrderedDict([(Path(f).parts[-3], mm.io.loadtxt(f, fmt=fmt, min_confidence=1)) for f in gtfiles])
+        ts = OrderedDict([(os.path.splitext(Path(f).parts[-1])[0], mm.io.loadtxt(f, fmt=fmt)) for f in tsfiles])    
 
-    logging.info('Found {} groundtruths and {} test files.'.format(len(gtfiles), len(tsfiles)))
-    logging.info('Available LAP solvers {}'.format(mm.lap.available_solvers))
-    logging.info('Default LAP solver \'{}\''.format(mm.lap.default_solver))
-    logging.info('Loading files.')
-    
-    gt = OrderedDict([(Path(f).parts[-3], mm.io.loadtxt(f, fmt=args.fmt, min_confidence=1)) for f in gtfiles])
-    ts = OrderedDict([(os.path.splitext(Path(f).parts[-1])[0], mm.io.loadtxt(f, fmt=args.fmt)) for f in tsfiles])    
+        mh = mm.metrics.create()    
+        accs, names = compare_dataframes(gt, ts)
+        
+        logging.info('Running metrics')
+        
+        summary = mh.compute_many(accs, names=names, metrics=mm.metrics.motchallenge_metrics, generate_overall=True)
+        print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
+        logging.info('Completed')
 
-    mh = mm.metrics.create()    
-    accs, names = compare_dataframes(gt, ts)
-    
-    logging.info('Running metrics')
-    
-    summary = mh.compute_many(accs, names=names, metrics=mm.metrics.motchallenge_metrics, generate_overall=True)
-    #summary = mh.compute_many(accs, names=names, metrics=['num_frames', 'mota', 'motp'], generate_overall=True)
-    print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
-    logging.info('Completed')
+        with open('./results.txt', 'a+') as f:
+                f.write('---------------------------------------------------------------------------------------------')
+                f.write(str(combine))
+                f.write('\r\n')
+                f.write(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
+                f.write('---------------------------------------------------------------------------------------------')
+                f.write('\r\n')
